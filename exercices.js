841df9e0ceb722e203825894cd875bd6264c3db8
Wagner <julian57280@hotmail.fr>
2025-01-22 09:22:56 +0100

    Affichage Conditionnel

--- exercices.js
+++ exercices.js
@@ -3,3 +3,8 @@ let potionsDeSoinsEnStock = 100;
 let prixPotionDeSoin = 15.99;
 let boutiqueOuverte = true;
 
+if (boutiqueOuverte) {
+    console.log("Bienvenue dans la boutique <La Bonne Potion> Aventurier ! 🎉 !");
+} else {
+    console.log("La boutique <La Bonne Potion> est fermée, revenez plus tard Aventurier ! 😴.");
+}
